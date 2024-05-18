3bb1f3bdca334e6d8c73e005551232bbfe61833a
eadiazm <eadiazm@gmail.com>
2024-05-18 09:44:50 -0500

    feat: Incrustar header y footer en la página, para incluir el menú.
    La forma de diseñar cada página cambia, pues toca agregar un componente <header> y <footer> al html de la página e importar la clase htmlLoader

--- js/htmlLoader.js
+++ js/htmlLoader.js
@@ -2,7 +2,12 @@ export class htmlLoader{
     static loadHTML(elementId, url) {
         fetch(url)
             .then(response => response.text())
-            .then(data => document.getElementById(elementId).innerHTML = data)
+            .then(data => document.querySelector(elementId).innerHTML = data)
             .catch(error => console.error('Error cargando el archivo:', error));
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    htmlLoader.loadHTML('header', 'header.html');
+    htmlLoader.loadHTML('footer', 'footer.html');
+});
