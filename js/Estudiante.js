9d6d8f89532a3256dde1533f1ef9e16c9072c722
eadiazm <eadiazm@gmail.com>
2024-05-18 19:10:17 -0500

    feat: Pagina de estudiantes semifuncionando

--- js/Estudiante.js
+++ js/Estudiante.js
@@ -1,3 +1,16 @@
 export class Estudiante{
+    constructor(documento, nombre, fechaNacimiento, sexo){
+        this.documento = documento
+        this.nombre = nombre
+        this.fechaNacimiento = fechaNacimiento
+        this.sexo = sexo
+        this.cursos = []
+    }
     
+    agregarCurso(curso){
+        this.cursos.push(curso)
+    }
+
+    
+
 }
